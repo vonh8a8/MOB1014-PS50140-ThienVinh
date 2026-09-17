f464865720d56aa155e82d1448e3d4ccfc410bbd
vonh8a8 <thvinh2543@gmail.com>
2026-09-17 06:45:34 +0100

    Hoan thanh lab 1 bai 1

---
+++ Lab1/Bai1_HelloJava/src/main/java/com/poly/bai1_hellojava/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.bai1_hellojava;
+
+/**
+ *
+ * @author Admin_Vinh
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Thien Vinh - PS50140");
+    }
+}
